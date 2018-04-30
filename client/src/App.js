b7d1c16bf8cf8d86b5404dd3346f47b272fb0e91
Richard Paez <richardp809@gmail.com>
2018-04-30 18:31:38 -0400

    padding added to the top of app

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <Header />
-        <p className="App-intro">
+        <p className="app-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
