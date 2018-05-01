b95147c0b35c61a042a109c856a0fb1bd8c3a185
Richard Paez <Richardp809@gmail.com>
2018-05-01 18:28:09 -0400

    added a default route for home inside of the main routing component

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import Header from './components/Header/header';
 import './App.css';
 
+import Main from './components/main';
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Header />
-        <p className="app-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Main />
       </div>
     );
   }
