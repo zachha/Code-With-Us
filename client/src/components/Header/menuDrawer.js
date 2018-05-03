3ef0031252c49c2455d4a42ea1f6b0a7d5529a92
Richard Paez <Richardp809@gmail.com>
2018-05-03 17:41:42 -0400

    populated the drawer with sample buttons

--- client/src/components/Header/menuDrawer.js
+++ client/src/components/Header/menuDrawer.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
 import Drawer from 'material-ui/Drawer';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import HomeIcon from '@material-ui/icons/Home';
+import CodeIcon from '@material-ui/icons/Code';
 
 const MenuDrawer = props => (
     <Drawer
         open={props.open}
         onClose={props.onCloseDrawer}>
-            test
+        <div className="drawer-main">
+            <h3 className="drawer-header">Code With Us</h3>
+        </div>
+        <List className="menu-items" component="nav">
+            <ListItem button>
+                <ListItemIcon>
+                    <HomeIcon />
+                </ListItemIcon>
+                <ListItemText primary="Home" />
+            </ListItem>
+            <ListItem button>
+                <ListItemIcon>
+                    <CodeIcon />
+                </ListItemIcon>
+                <ListItemText primary="About" />
+            </ListItem>
+        </List>
     </Drawer>
 );
 
