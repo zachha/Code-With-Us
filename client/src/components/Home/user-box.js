8570ab852fc03faf35759e6eef1955cc486da0d9
zachha <zachha@gmail.com>
2018-05-09 23:43:51 -0400

    user-box updated with fontawesome icon and css

--- client/src/components/Home/user-box.js
+++ client/src/components/Home/user-box.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import './user-box.css';
 import Paper from 'material-ui/Paper';
 import Typography from 'material-ui/Typography';
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
 
 const UserBox = props => {
     return (
         <Paper elevation={3} className="user-box">
-            <Typography variant="title">Hello! {props.username}</Typography>
+            <Typography variant="display3"><FontAwesomeIcon icon="id-card" size="2x" className="idCard"/>Hello {props.username}!</Typography>
         </Paper>
     )
 };
