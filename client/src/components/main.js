939b35591fcfea32e93353d8f23966a2107d81ba
Richard Paez <Richardp809@gmail.com>
2018-05-11 15:37:19 -0400

    added routes for the new forms

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Home/Home';
 import About from './About/About';
+import Login from './Login/Login';
+import Signup from './Signup/Signup';
 
 export default class Main extends Component {
 
@@ -11,6 +13,8 @@ export default class Main extends Component {
                 <Switch>
                     <Route exact path="/" render={() => <Home />}/>
                     <Route exact path="/about" render={() => <About />}/>
+                    <Route exact path="/login" render={() => <Login />}/>
+                    <Route exact path="/signup" render={() => <Signup />}/>
                     <Route exact path="/thread/:threadId" render={({match}) => <Home threadId={match.params.threadId}/>}/>
                     <Route exact path="/forum/:forumId" render={({match}) => <Home forumId={match.params.forumId}/>}/>
                 </Switch>
