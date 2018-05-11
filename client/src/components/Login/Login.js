caf5fa5231f3d048ad44509c30f5e36be9d49e27
Richard Paez <Richardp809@gmail.com>
2018-05-11 15:37:02 -0400

    added a log in form based on the signup form

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+
+import Paper from 'material-ui/Paper';
+
+import LoginForm from './loginForm';
+
+export default class Login extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            username : {
+                value : "",
+                valid : false,
+                error : false,
+                errorMessage : ""
+            },
+            password : {
+                value : "",
+                valid : false,
+                error : false,
+                errorMessage : ""
+            },
+            submitEnabled : true,
+            snackbar : {
+                open : false,
+             message : ""
+            }
+        };
+    }
+
+    onUsernameChange = e => {
+        if( e.target.value === "" ){
+            this.setState({
+                username: {
+                    ...this.state.username,
+                    value: e.target.value,
+                    valid: false,
+                    error: true,
+                    errorMessage: "A username is required"
+                }
+            });
+        } else {
+            this.setState({
+                username: {
+                    ...this.state.username,
+                    value: e.target.value,
+                    valid: true,
+                    error: false,
+                    errorMessage: ""
+                }
+            });
+        }
+    }
+
+    onPasswordChange = e => {
+        if( e.target.value === "" ){
+            this.setState({
+                password: {
+                    ...this.state.password,
+                    value: e.target.value,
+                    valid: false,
+                    error: true,
+                    errorMessage: "A password is required"
+                }
+            });
+        } else {
+            this.setState({
+                password: {
+                    ...this.state.password,
+                    value: e.target.value,
+                    valid: true,
+                    error: false,
+                    errorMessage: ""
+                }
+            });
+        }
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+
+        if( this.state.username.valid && this.state.password.valid ){
+            let credentials = {
+                username : this.state.username.value.toLowerCase(),
+                password : this.state.password.value
+            }
+
+            this.setState({
+                submitEnabled: false
+            });
+
+            //axios request here with the credentials
+        }
+    }
+
+    render(){
+        return (
+        <Paper elevation={3} className="form-box">
+            <LoginForm 
+                {...this.state}
+                onUsernameChange={this.onUsernameChange}
+                onPasswordChange={this.onPasswordChange}
+                onSubmit={this.onSubmit}/>
+        </Paper>)
+    }
+}
