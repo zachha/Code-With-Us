3a2c215d5206694d19ca93372640509971cbeb00
Richard Paez <Richardp809@gmail.com>
2018-05-11 15:37:38 -0400

    updated the drawer to auto close and send users to the log in

--- client/src/components/Header/menuDrawer.js
+++ client/src/components/Header/menuDrawer.js
@@ -5,6 +5,7 @@ import Drawer from 'material-ui/Drawer';
 import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import HomeIcon from '@material-ui/icons/Home';
 import CodeIcon from '@material-ui/icons/Code';
+import PersonIcon from '@material-ui/icons/Person';
 
 const MenuDrawer = props => (
     <Drawer
@@ -14,7 +15,7 @@ const MenuDrawer = props => (
             <h3 className="drawer-header">Code With Us</h3>
         </div>
         <List className="menu-items" component="nav">
-            <Link to='/'>
+            <Link to='/' onClick={props.onCloseDrawer}>
                 <ListItem button>
                     <ListItemIcon>
                         <HomeIcon />
@@ -22,7 +23,15 @@ const MenuDrawer = props => (
                     <ListItemText primary="Home" />
                 </ListItem>
             </Link>
-            <Link to='/about'>
+            <Link to='/login' onClick={props.onCloseDrawer}>
+                <ListItem button>
+                    <ListItemIcon>
+                        <PersonIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Login" />
+                </ListItem>
+            </Link>
+            <Link to='/about' onClick={props.onCloseDrawer}>
                 <ListItem button>
                     <ListItemIcon>
                         <CodeIcon />
