d289a1c1f8a6686c5198fc112ce90b8eb0779dc8
zachha <zachha@gmail.com>
2018-05-15 19:12:39 -0400

    update api and controller to findAllPosts and findAllThreads for user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,6 @@
 const db = require('../models');
 
 module.exports = {
-
   // function for user to create username and password for user authentication
   newUser: (req, res) => {
     db.User.create({
@@ -17,7 +16,7 @@ module.exports = {
   },
 
   // allows user to update their password
-  updatePassword: (req,res) => {
+  updatePassword: (req, res) => {
     db.User.update(
       {
         password: req.body.password
@@ -25,18 +24,20 @@ module.exports = {
       {
         where: {
           id: req.user.id,
-          email:req.user.email
+          email: req.user.email
         }
       }
     )
       .then(result =>
-        console.log("User: " + req.user.username + "'s password was successfully changed!")
+        console.log(
+          "User: " + req.user.username + "'s password was successfully changed!"
+        )
       )
       .catch(err => console.log(err));
   },
 
   // allows user to update their stored email address
-  updateEmail: (req,res) => {
+  updateEmail: (req, res) => {
     db.User.update(
       {
         email: req.body.email
@@ -48,41 +49,40 @@ module.exports = {
       }
     )
       .then(result =>
-        console.log("User: " + req.user.username + "'s email was successfully changed!")
+        console.log(
+          "User: " + req.user.username + "'s email was successfully changed!"
+        )
       )
       .catch(err => console.log(err));
   },
 
   // finds all users and sorts them by most reputaiton to least
-  findAll: (req,res) => {
+  findAll: (req, res) => {
     db.User.findAll({
       order: [["reputation", "DESC"]],
-      raw:true
+      raw: true
     })
-    .then(users => res.json(users))
-    .catch(err => console.log(err));
+      .then(users => res.json(users))
+      .catch(err => console.log(err));
   },
 
   //finda a user from a saved token if they previously logged in
-  findFromToken: (req,res) => {
-     db.User.findOne({
-       where:{
-         id:req.user.id,
-         username:req.user.username
-       }
-     }).then(user => res.json(user));
+  findFromToken: (req, res) => {
+    db.User.findOne({
+      where: {
+        id: req.user.id,
+        username: req.user.username
+      }
+    }).then(user => res.json(user));
   },
 
   // Allows user to find another user by their user name
   findUser: (req, res) => {
     db.User.findOne({
       where: {
-        id:req.params.id
-            },
-      include: [
-        { model: db.Post },
-        { model: db.Thread }
-      ]
+        id: req.params.id
+      },
+      include: [{ model: db.Post }, { model: db.Thread }]
     })
       .then(user => {
         console.log(user.get({ plain: true }));
@@ -93,29 +93,61 @@ module.exports = {
 
   // Finds all of a user's posts
   findAllPosts: (req, res) => {
-      db.User.findAll({
-        where: {
-          id: req.params.userId || req.user.id
-        },
-        include: [
-          {
-            model: db.Post
-          }
-        ]
+    db.User.findAll({
+      where: {
+        id: req.params.userId || req.user.id
+      },
+      include: [
+        {
+          model: db.Post,
+          include: [ db.Thread ]
+        }
+      ]
+    })
+      .then(userPosts => {
+        if (userPosts.postCount > 0) {
+          console.log(userPosts.get({ plain: true }).Posts);
+          res.json(userPosts.get({ plain: true }).Posts);
+        } else {
+          console.log("User has not made any posts");
+          res.json("User has not made any posts");
+        }
+        
       })
-        .then(userPosts => {
-            console.log((userPosts.get({ plain: true })).Posts);
-            res.json((userPosts.get({ plain: true })).Posts);
-      })
-        .catch(err => console.log(err));
+      .catch(err => console.log(err));
   },
 
-    // Allows users to increase rep of specified user for helpful answers
+  // Finds all of a user's threads
+  findAllThreads: (req, res) => {
+    db.User.findAll({
+      where: {
+        id: req.params.userId || req.user.id
+      },
+      include: [
+        {
+          model: db.Thread,
+          include: [db.Subforum]
+        }
+      ]
+    })
+      .then(userThreads => {
+        if (userThreads.Threads) {
+           console.log(userThreads.get({ plain: true }).Threads);
+           res.json(userThreads.get({ plain: true }).Threads);
+        } else {
+          console.log("User has not created any threads!");
+          res.json("User has not created any threads");
+        }
+      })
+      .catch(err => console.log(err));
+  },
+
+  // Allows users to increase rep of specified user for helpful answers
   increaseReputation: (req, res) => {
-      db.User.findById(req.body.userId)
-      .then(user => 
-         user.increment({
-            reputation: 1
+    db.User.findById(req.body.userId)
+      .then(user =>
+        user.increment({
+          reputation: 1
         })
       )
       .then(user => {
@@ -125,12 +157,12 @@ module.exports = {
       .catch(err => console.log(err));
   },
 
-  // Allows users to decrease rep of other users 
+  // Allows users to decrease rep of other users
   decreaseReputation: (req, res) => {
-      db.User.findById(req.body.userId)
-      .then(user => 
-         user.decrement({
-            reputation: 1
+    db.User.findById(req.body.userId)
+      .then(user =>
+        user.decrement({
+          reputation: 1
         })
       )
       .then(user => {
@@ -138,24 +170,30 @@ module.exports = {
         res.json(user.get({ plain: true }));
       })
       .catch(err => console.log(err));
-  },      
+  },
 
   // allows a mod or the specified user to delete their information
-  deleteUser: (req,res) => {
-    db.user.findOne({
-      where:{id:req.user.id,email:req.user.email}
-    })
-    .then(user =>{
-      if(user.isModerator || user.id===req.params.id){
-        db.User.destroy({
+  deleteUser: (req, res) => {
+    db.user
+      .findOne({
+        where: { id: req.user.id, email: req.user.email }
+      })
+      .then(user => {
+        if (user.isModerator || user.id === req.params.id) {
+          db.User.destroy({
             where: {
-                id: req.params.id,
+              id: req.params.id
             }
-        })      
-      .then(user => {console.log('User '+user.id+' has been deleted');res.send("success!");})
-      .catch(err => console.log(err))
-      }else{res.end(res.writeHead(401,{"error":"You don't have permission!"}))}
-    })
+          })
+            .then(user => {
+              console.log("User " + user.id + " has been deleted");
+              res.send("success!");
+            })
+            .catch(err => console.log(err));
+        } else {
+          res.end(res.writeHead(401, { error: "You don't have permission!" }));
+        }
+      });
   }
 };
 
--- routes/api/userAPI.js
+++ routes/api/userAPI.js
@@ -9,12 +9,17 @@ router.route('/')
 router.get("/fromtoken",userController.findFromToken);//finds a user from a saved token   
 
 router.route("/:id")
-    .get(userController.findUser) // finds specific user
+  .get(userController.findUser) // finds specific user
+  .put(userController.updatePassword) // updates password
+  .put(userController.updateEmail) // updates email
+  .put(userController.increaseReputation) // increases specified user's reputation
+  .put(userController.decreaseReputation) // decreases specified user's reputation
+  .delete(userController.deleteUser); // deletes user    
+
+router.route("/posts/:id")
     .get(userController.findAllPosts) // finds all of a user's posts
-    .put(userController.updatePassword) // updates password
-    .put(userController.updateEmail) // updates email
-    .put(userController.increaseReputation) // increases specified user's reputation
-    .put(userController.decreaseReputation) // decreases specified user's reputation
-    .delete(userController.deleteUser); // deletes user    
+
+router.route("/threads/:id")
+    .get(userController.findAllThreads) // finds all of a user's threads
 
 module.exports = router;
