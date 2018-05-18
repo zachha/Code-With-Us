fdeabd2c2706d650e8721919a81573915c0be9e9
Richard Paez <Richardp809@gmail.com>
2018-05-18 11:21:15 -0400

    fixed header opening the app drawer

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,12 +8,11 @@ import MenuDrawer from './menuDrawer';
 const Header = props => {
     return (
         <React.Fragment>
-            <div 
-                className="menu-bar"
-                onClick={props.onOpenDrawer} >
-                <MenuIcon 
-                    className="menu-icon" />
+            <div className="menu-bar">
                 <h2>Code With Us</h2>
+                <MenuIcon
+                    className="menu-icon"
+                    onClick={props.onOpenDrawer}/>
             </div>
             <MenuDrawer 
                 open={props.isMenuOpen}
