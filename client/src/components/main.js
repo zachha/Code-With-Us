20a9f68ca94ab64f52ca0bc28d74813f55e8a501
Richard Paez <Richardp809@gmail.com>
2018-05-18 11:30:07 -0400

    main is now a functional stateless component, main state  is now in App

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import Home from './Home/Home';
 import User from './User/User';
 import About from './About/About';
@@ -7,20 +8,35 @@ import Login from './Login/Login';
 import Signup from './Signup/Signup';
 
 
-export default class Main extends Component {
-    render(){
-        return <div className="content-main">
-            <Switch>
-              <Route exact path="/" render={() => <Home user={this.props.user} />} />
-              <Route exact path="/about" render={() => <About />} />
-              <Route exact path="/login" render={() => <Login   loginCallback={this.handleLogin}
-                                                                user={this.props.user} /> } />
-              <Route exact path="/signup" render={() => <Signup />} />
-              <Route exact path="/user" render={() => <User user={this.props.user} />} />
-              <Route exact path="/user/:userId" render={({ match }) => <User user={this.props.user} userId={match.params.userId} />} />
-              <Route exact path="/thread/:threadId" render={({ match }) => <Home user={this.props.user} threadId={match.params.threadId} />} />
-              <Route exact path="/forum/:forumId" render={({ match }) => <Home user={this.props.user} forumId={match.params.forumId} />} />
-            </Switch>
-          </div>;
-    }
-};
+const Main = (props) => (
+    <div className="content-main">
+        <Switch>
+            <Route  exact path="/"
+                    render={() => <Home     user={props.user} />} />
+
+            <Route  exact path="/about"
+                    render={() => <About />} />
+
+            <Route  exact path="/login"
+                    render={() => <Login    loginCallback={props.handleLogin}
+                                            user={props.user} /> } />
+
+            <Route  exact path="/signup"
+                    render={() => <Signup />} />
+
+            <Route  exact path="/user"
+                    render={() => <User     user={props.user} />} />
+
+            <Route  exact path="/user/:userId"
+                    render={({ match }) => <User    user={props.user}
+                                                    userId={match.params.userId} />} />
+
+            <Route  exact path="/thread/:threadId" 
+                    render={({ match }) => <Home user={props.user} threadId={match.params.threadId} />} />
+
+            <Route  exact path="/forum/:forumId" 
+                    render={({ match }) => <Home user={props.user} forumId={match.params.forumId} />} />
+        </Switch>
+    </div>);
+
+export default Main;
